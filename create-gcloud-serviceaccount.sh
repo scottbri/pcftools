805d651c83d0ded3527309040829a6c3f475e78b
Scott Brightwell <scott@brightwell.org>
2018-05-11 18:42:42 -0400

    added create-gcloud-serviceaccount.sh

---
+++ create-gcloud-serviceaccount.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "In which GCP Project will this account reside:"
+read PROJECT
+echo ""
+echo "What username do you want for this service account:"
+read USERNAME
+echo ""
+echo "What filename will the json key be written:"
+read FILENAME
+echo ""
+
+gcloud iam service-accounts create ${USERNAME} --display-name "${USERNAME} Service Account"
+gcloud iam service-accounts keys create "${FILENAME}" --iam-account "${USERNAME}@${PROJECT}.iam.gserviceaccount.com"
+gcloud projects add-iam-policy-binding ${PROJECT} --member 'serviceAccount:${USERNAME}@${PROJECT}.iam.gserviceaccount.com' --role 'roles/owner'
