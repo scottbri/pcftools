84288e4bf2c6a9223ae698fd1155db6ca4afccb4
Scott Brightwell <scott@brightwell.org>
2019-11-20 16:32:54 -0600

    image builder changes

---
+++ setup-shell-image.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+BINDIR="${HOME}/bin"
+GITUSER="Scott Brightwell"
+GITEMAIL="scott@brightwell.org"
+
+INSTALL_BINARIES=true
+SETUP_GIT=true
+
+function setup-git {
+	git config --global user.name "${GITUSER}"
+	git config --global user.email "${GITEMAIL}"
+}
+
+function setup-bash-it {
+	git clone http://github.com/scottbri/bash-it
+	mv bash-it ${HOME}/.bash-it
+	${HOME}/.bash-it/install.sh --silent
+	sed -i.bak 's/BASH_IT_THEME=.*/BASH_IT_THEME=scott/' ${HOME}/.bashrc
+}
+
+function install-binaries {
+	mkdir ${BINDIR} || exit 1
+	wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
+	apt-get install unzip
+	unzip terraform_0.11.13_linux_amd64.zip
+	mv terraform ${BINDIR}/
+	chmod +x ${BINDIR}/terraform
+
+	OMLINUX=om-linux-4.3.0
+	#wget https://github.com/pivotal-cf/om/releases/download/0.54.0/om-linux
+	wget https://github.com/pivotal-cf/om/releases/download/4.3.0/om-linux-4.3.0
+	mv $OMLINUX ${BINDIR}/
+	chmod +x ${BINDIR}/$OMLINUX
+	ln -s ${BINDIR}/$OMLINUX ${BINDIR}/om
+
+	wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+	mv jq-linux64 ${BINDIR}/
+	chmod +x ${BINDIR}/jq-linux64
+	ln -s ${BINDIR}/jq-linux64 ${BINDIR}/jq
+
+	BOSHCLI=bosh-cli-6.1.1-linux-amd64
+	#wget https://github.com/cloudfoundry/bosh-cli/releases/download/v5.4.0/bosh-cli-5.4.0-linux-amd64
+	wget https://github.com/cloudfoundry/bosh-cli/releases/download/v6.1.1/bosh-cli-6.1.1-linux-amd64
+	mv $BOSHCLI ${BINDIR}/
+	chmod +x ${BINDIR}/$BOSHCLI
+	ln -s ${BINDIR}/$BOSHCLI ${BINDIR}/bosh
+
+	wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | apt-key add -
+	#echo "deb https://packages.cloudfoundry.org/debian stable main" | tee /etc/apundry-cli.list.d/cloudfoundry-cli.list
+	apt-get update
+	apt-get install cf-cli
+
+	CREDHUB=credhub-linux-2.6.1.tgz
+	#wget https://github.com/cloudfoundry-incubator/credhub-cli/releases/download/2.2.1/credhub-linux-2.2.1.tgz
+	wget https://github.com/cloudfoundry-incubator/credhub-cli/releases/download/2.6.1/credhub-linux-2.6.1.tgz
+	tar zxvf $CREDHUB
+	mv credhub ${BINDIR}/
+	chmod +x ${BINDIR}/credhub
+
+	apt-get install build-essential ruby-dev ruby
+	gem install cf-uaac
+
+	wget https://github.com/direnv/direnv/releases/download/v2.19.2/direnv.linux-amd64
+	mv direnv.linux-amd64 ${BINDIR}/
+	chmod +x ${BINDIR}/direnv.linux-amd64
+	ln -s ${BINDIR}/direnvlinux-amd64 ${BINDIR}/direnv
+
+	wget https://github.com/concourse/concourse/releases/download/v4.2.3/fly_linux_amd64
+	mv fly_linux_amd64 ${BINDIR}/fly
+	chmod +x ${BINDIR}/fly
+
+	curl -L https://aka.ms/InstallAzureCli | bash
+	
+	PIVNET=pivnet-linux-amd64-0.0.74
+	wget https://github.com/pivotal-cf/pivnet-cli/releases/download/v0.0.74/pivnet-linux-amd64-0.0.74
+	mv $PIVNET ${BINDIR}/
+	chmod +x ${BINDIR}/$PIVNET
+	ln -s ${BINDIR}/$PIVNET ${BINDIR}/pivnet
+}
+
+$INSTALL_BINARIES && (echo "Installing binaries:"; install-binaries)
+$SETUP_GIT && (echo "Setting up git"; setup-git)
+$SETUP_BASH && (echo "Setting up bash-it"; setup-bash-it)
